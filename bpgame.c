921726219dfdbcdd66e9f14c67d06b8781538b6a
Hamziniii <hamzagaziuddin04@gmail.com>
2021-10-08 13:45:31 -0500

    Added bp_destory

--- bpgame.c
+++ bpgame.c
@@ -21,6 +21,7 @@ struct bpgame {
 /*** IMPLEMENTATION OF bp_XXXX FUNCTIONS HERE  ****/
 extern int isRandInit = 0;
 
+// Sets the seed if it hasnt been set before
 void randInit() {
    if(!isRandInit) {
       isRandInit = 1;
@@ -33,6 +34,7 @@ BPGame* bp_create(int nrows, int ncols) {
       fprintf(stderr, "%s", "Error with nrows (%i) or ncols (%i) in bp_create!\n", nrows, ncols);
       return NULL;
    }
+   randInit();
 
    BPGame* game = malloc(sizeof(BPGame)); // allocate space for the game 
    game->nrows = nrows; // sets the rows
@@ -55,7 +57,8 @@ BPGame* bp_create(int nrows, int ncols) {
    }
 
    for(int r = 0; r < game->nrows; r++) // for each row
-      game->grid[r] = grid[r * game->nrows]; // set it to the the corresponding place in grid var   
+      game->grid[r] = grid[r * game->nrows]; // set it to the the corresponding place in grid var  
+   return game; 
 }
 
 // the same thing as above, but getting vlaues from a matrix instead of random
@@ -78,4 +81,15 @@ BPGame* bp_create_from_mtx(char mtx[][MAX_COLS], int nrows, int ncols) {
    for(int i = 0; i < game->nrows; i++)
       for(int j = 0; j < game->ncols; j++)
          game->grid[i][j] = mtx[i][j]; // for each value in the matrix, set it into the game grid
+   return game;
+}
+
+// Free everything / set everything to 0
+void bp_destroy(BPGame* b) {
+   b->ncols = 0;
+   b->nrows = 0;
+   b->gridHistorySize = 0;
+   free(b->gridHistory);
+   free(b->grid);
+   free(b);
 }
